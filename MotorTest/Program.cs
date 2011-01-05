151e6995e1539168583005e7d9ec87ee392be08f
slmiller <slmiller@f791fe0b-c856-0410-8075-ba0306ed6e46>
2011-01-05 15:45:12 +0000

    Further refinements.  Still not working.  Need to find a way to alias pins.
    git-svn-id: http://www.qwindelzorf.com/programming_projects/AFMotorShield@118 f791fe0b-c856-0410-8075-ba0306ed6e46

--- MotorTest/Program.cs
+++ MotorTest/Program.cs
@@ -12,9 +12,35 @@ namespace MotorTest
     {
         public static void Main()
         {
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
             DCMotor dc = new DCMotor(MotorShield.MotorHeaders.M1);   // A DC motor on header M1
-            dc.Run(DCMotor.MotorDirection.Release); // Disable the motor
-            dc.SetSpeed(50); // 50% speed
+            dc.SetSpeed(0);
+            dc.Run(DCMotor.MotorDirection.Forward); // Disable the motor
+
+            int speed = 0;
+            int increment = 1;
+
+            while (true)
+            {
+                dc.SetSpeed((uint)speed);
+                
+                speed += increment;
+                if(speed > 100)
+                {
+                    speed = 100;
+                    increment = -1;
+                }
+                else if (speed < 0)
+                {
+                    speed = 0;
+                    increment = 1;
+                }
+
+                led.Write((increment > 0) ? true : false);
+                Debug.Print(speed + "\n");
+
+                Thread.Sleep(50);
+            }
         }
 
     }
